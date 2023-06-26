324b4e12786ceb6babfb48471e1bef99acc9c1f5
SpaceRasser <rasser31.j@gmail.com>
2023-06-26 21:15:35 +0400

    rasser v4
    Сделал меню игры

--- RGM-Game/Assets/Script/character_had.cs
+++ RGM-Game/Assets/Script/character_had.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 public class character_had : MonoBehaviour
 {
     public float speed;
-
-    private Rigidbody2D rb;
-    private Vector2 moveVelocity;
+    private Rigidbody2D rb2d;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
-
 
     void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveInput.normalized * speed;
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb2d.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+
+        // Try out this delta time method??
+        //rb2d.transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
     }
 
-    private void FixedUpdate()
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
     {
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
 }
